48abbea00897b578311fb5e9311103217b83f25f
thiseni0224 <rajakarunapatabadige@gmail.com>
2026-07-24 10:21:22 +0530

    Update shop layout and bundled assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/plugins/bootstrap-select.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/plugins/bootstrap-datepicker.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
+    @stack('page-styles')
 </head>
 
 
@@ -112,7 +113,7 @@
                         <div class="header-info">
                             <div class="logo">
                                 <a href="{{ route('home') }}">
-                                    <img src="{{ asset('assets/images/logo/logo.webp') }}" alt="Ecommerce Logo Images">
+                                    <img src="{{ asset('assets/images/logo/logo-nbc2.webp') }}" alt="NBC Logo Images">
                                 </a>
                             </div>
                         </div>
@@ -1765,13 +1766,6 @@
                             </a>
                         </li>
 
-                        <li class="rbt-access-box rbt-scroll-trigger fade_in animation-order-4 tooltips tooltip-distance-lg  d-none d-lg-flex" data-tooltip="Compare" data-tooltip-position="bottom">
-                            <a class="rbt-round-btn has-rbt-md-fsize" href="#" data-bs-toggle="modal" data-bs-target="#compareviewModal">
-                                <i class="fa-regular fa-code-compare"></i>
-                                <div class="access-box-count">6</div>
-                            </a>
-                        </li>
-
                         <li class="rbt-access-box rbt-scroll-trigger fade_in animation-order-4 rbt-wishlist d-none d-lg-flex tooltips tooltip-distance-lg" data-tooltip="Wishlist" data-tooltip-position="bottom">
                             <a class="rbt-round-btn has-rbt-md-fsize" href="wishlist.html">
                                 <i class="fa-regular fa-heart"></i>
@@ -2311,7 +2305,7 @@
                     <div class="header-info d-xl-block d-none">
                         <div class="logo rbt-logo-height-sm">
                             <a href="{{ route('home') }}">
-                                <img src="{{ asset('assets/images/logo/logo.webp') }}" alt="Ecommerce Logo Images">
+                                <img src="{{ asset('assets/images/logo/logo-nbc2.webp') }}" alt="NBC Logo Images">
                             </a>
                         </div>
                     </div>
@@ -2330,7 +2324,7 @@
             <div class="header-info d-xl-none d-block">
                 <div class="logo">
                     <a href="{{ route('home') }}">
-                        <img src="{{ asset('assets/images/logo/logo.webp') }}" alt="Ecommerce Logo Images">
+                        <img src="{{ asset('assets/images/logo/logo-nbc2.webp') }}" alt="NBC Logo Images">
                     </a>
                 </div>
             </div>
@@ -3721,14 +3715,6 @@
                         </a>
                     </li>
 
-                    <li class="rbt-access-box rbt-scroll-trigger fade_in animation-order-4 tooltips tooltip-distance-lg  d-none d-lg-flex" data-tooltip="Compare" data-tooltip-position="bottom">
-                        <a class="rbt-round-btn has-rbt-md-fsize" href="#" data-bs-toggle="modal" data-bs-target="#compareviewModal">
-                            <i class="fa-regular fa-code-compare"></i>
-                            <div class="access-box-count">6</div>
-                        </a>
-                    </li>
-
-
                     <li class="rbt-access-box rbt-scroll-trigger fade_in animation-order-5 rbt-wishlist d-none d-lg-flex tooltips tooltip-distance-lg" data-tooltip="Wishlist" data-tooltip-position="bottom">
                         <a class="rbt-round-btn has-rbt-md-fsize" href="#!" data-bs-toggle="modal" data-bs-target="#wishlistModal">
                             <i class="fa-regular fa-heart"></i>
@@ -4406,7 +4392,7 @@
                 <div class="col-xxl-3 col-xl-3 col-lg-2 col-md-12 col-12 mt--24">
                     <div class="logo mx-auto mx-xl-0">
                         <a href="{{ route('home') }}">
-                            <img src="{{ asset('assets/images/logo/logo.webp') }}" alt="Unimart Logo">
+                            <img src="{{ asset('assets/images/logo/logo-nbc2.webp') }}" alt="NBC Logo">
                         </a>
                     </div>
                 </div>
@@ -4460,7 +4446,7 @@
                         <div class="rbt-login-form-top">
                             <div class="logo">
                                 <a href="{{ route('home') }}">
-                                    <img src="{{ asset('assets/images/logo/logo.webp') }}" alt="Ecommerce Logo Images">
+                                    <img src="{{ asset('assets/images/logo/logo-nbc2.webp') }}" alt="NBC Logo Images">
                                 </a>
                             </div>
                             <h3 class="rbt-title rbt-text-bold mb--16 h6" id="signinModalLabel">Sign In To Proceed</h3>
@@ -4670,7 +4656,7 @@
                         <div class="rbt-login-form-top">
                             <div class="logo">
                                 <a href="{{ route('home') }}">
-                                    <img src="{{ asset('assets/images/logo/logo.webp') }}" alt="Ecommerce Logo Images">
+                                    <img src="{{ asset('assets/images/logo/logo-nbc2.webp') }}" alt="NBC Logo Images">
                                 </a>
                             </div>
                             <h3 class="rbt-title rbt-text-bold mb--16 h6" id="signupModalLabel">Create an Account</h3>
